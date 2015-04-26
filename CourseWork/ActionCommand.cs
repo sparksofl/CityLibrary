b1d987750a9ac6b8421383aceea4999bc72e6a5f
Mary Solodovnick <gimiro42@gmail.com>
2015-04-26 18:33:53 +0300

    Filters added.

---
+++ CourseWork/ActionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CourseWork
+{
+    public class ActionCommand : ICommand
+    {
+        // Класс команды без параметров
+        private Action _action;
+        private bool _isExecutable;
+
+        public bool IsExecutable
+        {
+            get { return _isExecutable; }
+            set
+            {
+                _isExecutable = value;
+                if (CanExecuteChanged == null)
+                    return;
+                CanExecuteChanged(this, new EventArgs());
+            }
+        }
+
+        public ActionCommand(Action action)
+        {
+            _action = action;
+        }
+
+        // Предикат показывает можно ли запускать команды при заданном аргументе.
+        public bool CanExecute(object parameter)
+        {
+            return IsExecutable;
+        }
+
+        // Что должна выполнять команда
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ CourseWork/LastBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork
+{
+    class LastBook
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+    }
+}
